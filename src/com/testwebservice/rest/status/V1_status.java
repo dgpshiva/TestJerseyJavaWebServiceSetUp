8b81bee240969332f69bcce609380ee9c624788e
Shiva <dgpshiva@users.noreply.github.com>
2016-10-06 21:09:11 -0500

    Test Jersey Java web service

---
+++ src/com/testwebservice/rest/status/V1_status.java
@@ -0,0 +1,15 @@
+package com.testwebservice.rest.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status/")
+public class V1_status {
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle()
+	{
+		return "<p>From inside Java Web Service</p>";
+	}
+}
